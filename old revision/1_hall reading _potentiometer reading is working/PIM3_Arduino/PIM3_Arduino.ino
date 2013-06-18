be7c6e3e7461adde809043c5c1ef3297f68b0c3e
Musadjan <musadjan@gmail.com>
2013-06-18 11:05:18 +0200

    revision
    Signed-off-by: Musadjan <musadjan@gmail.com>

---
+++ old revision/1_hall reading _potentiometer reading is working/PIM3_Arduino/PIM3_Arduino.ino
@@ -0,0 +1,228 @@
+
+
+
+// --------------motor A I/O for Lift---------
+const int DIR_A=22;
+const int PWM_A=9;
+//----------------motor B I/O --------
+const int DIR_B=23;
+const int PWM_B=10;
+//---------------motor C I/O for hand grip-----------
+const int DIR_C=50;
+const int PWM_C=6;
+
+//-----------------
+const int hallPinA_1 = 2;     // A_the number of the hall effect sensor pin
+const int hallPinA_2 = 3;    //// A_the number of the hall effect sensor pin, it is digital input only referens direction
+// variables will change:
+int hall_A1_counter = 0;          // variable for reading the hall sensor status
+int hall_A2_counter = 0; 
+int PIN=13;
+int status=LOW;
+int hall_counter=0;
+int hall_real=0;
+int forward, backward, calibrate;
+const int start_position=100;
+int duty_c_backward=0;
+int duty_c_forward=0;
+char My_position[] = "arduino";
+enum my_position {
+  MY_POSITION_T0,
+  MY_POSITION_T1,
+  MY_POSITION_T2,
+};
+
+enum my_position my_current_position;
+
+//----------------------------------Sensor_with potentiometer 20K----
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+// ------------------I/O setup---------------------------
+int up_button=12;   // Up button
+int down_button=11; // down button
+int cal_button=10; // calibrate the hand guide to start position
+//------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(57600);
+  pinMode(up_button, INPUT); 
+  pinMode(down_button, INPUT);
+  pinMode(cal_button, INPUT); 
+  pinMode(PIN, OUTPUT);
+  pinMode(DIR_C,OUTPUT);
+  
+        // leave other bits as set by arduino init() in wiring.c
+//        byte mask = B11111001;
+//        TCCR4B &= mask; // TCCR4B is now xxxxx000
+//        //
+//        // set CS22:20 in TCCR4B see p 156 of datasheet
+//        TCCR4B |= (0<<CS22) | (0<<CS21) | (1<<CS20); // same as TCCR4B |= B00000001; TCCR4B is now xxxxx001
+////  
+////  
+        TCCR2B = TCCR2B & 0b11111000 | 0x01;
+        TCCR4B = TCCR4B & 0b11111000 | 0x02;
+}
+
+void loop()
+{
+  
+  sensorValue = analogRead(sensorPin);      // Read value from the potentiometer 
+  attachInterrupt(0, hall_A, RISING);
+  calibrate=digitalRead(cal_button);        //Reading the cal-button
+  forward=digitalRead(up_button);           //Reading the forward button 
+  backward=digitalRead(down_button);        //Reading the backward button
+  float handguide_position = sensorValue/4;
+    
+                                                  if(calibrate==HIGH) 
+                                                  {
+                                                    if (handguide_position< start_position)
+                                                    {
+                                                      
+                                                      digitalWrite(DIR_C, HIGH);
+                                                      analogWrite(PWM_C, 25);
+                                                                  }
+                                                    if (handguide_position> start_position)
+                                                    {
+                                                      
+                                                      digitalWrite(DIR_C, LOW);
+                                                      analogWrite(PWM_C, 25);
+                                                                }
+                                                                //if (handguide_position>100 && handguide_position<120 ) analogWrite(PWM_C, 0);
+                                                    if (handguide_position>100 && handguide_position<105 ) analogWrite(PWM_C, 0);
+                                                            }
+              if (handguide_position>100 && handguide_position<105 ) analogWrite(PWM_C, 0);
+                if (forward==HIGH)
+               { 
+                 forward_A();
+                 //attachInterrupt(0, hall_A1plus, FALLING);
+                 //Serial.println("forward");
+                 if (hall_A1_counter>=200) 
+                  {
+                    digitalWrite(PIN, HIGH);
+                          }    
+                               }
+   
+              
+             if (backward==HIGH)
+              {
+              //  Serial.println("backward");
+                backward_A();
+               // attachInterrupt(0, hall_A1minus, FALLING);
+                if (hall_A1_counter<=200) 
+                {
+                  digitalWrite(PIN, LOW);
+                       } 
+                            }
+                            
+          if (backward==LOW && forward==LOW) 
+          {
+            stop();
+            duty_c_forward=0;
+            duty_c_backward=0;
+          }
+
+          if (hall_A1_counter<=300) my_current_position = MY_POSITION_T0;
+          if (hall_A1_counter>300&& hall_A1_counter<=1000) my_current_position = MY_POSITION_T1;
+          if (hall_A1_counter>1001&& hall_A1_counter<=3000) my_current_position = MY_POSITION_T2;
+          
+     switch (my_current_position) {
+  case MY_POSITION_T0:    // your hand is on the sensor
+    Serial.println("T0");
+    break;
+  case MY_POSITION_T1:    // your hand is close to the sensor
+    Serial.println("   T1");
+    break;
+  case MY_POSITION_T2:    // your hand is a few inches from the sensor
+    Serial.println("         T2");
+    break;
+  default:    // your hand is nowhere near the sensor
+    Serial.println("                       T3");
+    break;
+  }           
+   Serial.println(handguide_position);       
+   Serial.println(hall_A1_counter);
+  // Serial.println(duty_c_backward);
+}
+
+//----------------------------------------------Functions listed below------------------------------------------------------
+void hall_A()                        　// interrupt 0 triggs this 
+{ 
+  
+  if (digitalRead(hallPinA_2)==HIGH)
+  {
+   hall_A1_counter++;
+}
+else hall_A1_counter--;
+}
+
+//------------------function Motor A forward-------------------------------------
+void forward_A()
+{
+  digitalWrite(DIR_A, HIGH);
+  if (duty_c_forward<255) 
+  {
+  duty_c_forward++;
+  analogWrite(PWM_A, duty_c_forward);
+  delay(2);
+  }
+}
+
+//------------------function Motor A backward forward----
+void backward_A()
+{
+  digitalWrite(DIR_A, LOW);
+  if (duty_c_backward<255)
+ { 
+   duty_c_backward++;
+  analogWrite(PWM_A, duty_c_backward);
+  delay(2);
+ }
+}
+
+//------------------function Motor B forward----------------------------------
+void forward_B()
+{
+  digitalWrite(DIR_B, HIGH);
+  analogWrite(PWM_B, duty_c_forward);
+}
+
+//------------------function Motor B backward ------
+void backward_B()
+{
+  digitalWrite(DIR_B, LOW);
+  analogWrite(PWM_B, duty_c_backward);
+}
+
+//------------------function Motor C forward----------------------------------
+void forward_C()
+{
+  digitalWrite(DIR_C, HIGH);
+  analogWrite(PWM_C, 50);
+}
+
+//------------------function Motor C backward ------
+void backward_C()
+{
+  digitalWrite(DIR_C, LOW);
+  analogWrite(PWM_C, duty_c_backward);
+}
+
+//------------------Stop A&B Motor ------
+
+void stop()
+{
+ 
+  while (duty_c_forward >= 1) {
+    analogWrite(PWM_A, duty_c_forward--);
+    delay(2);
+  }
+ 
+  while (duty_c_backward >= 1) {
+    analogWrite(PWM_A, duty_c_backward--);
+    delay(2);
+  }
+}
+
+
